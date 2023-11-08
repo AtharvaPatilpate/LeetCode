61dc95f3ed14e1fed763fe17801cf46aa0c299b7
Atharva Patilpate <atharva.patilpate@gmail.com>
2023-11-08 08:46:23 +0530

    Time: 2 ms (36.53%) | Memory: 6.3 MB (5.30%) - LeetSync

--- DailyProblem/3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ DailyProblem/3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
 
-        if ((sx == fx) && (sy == fy)) {
-            return t==1?false:true;
-        } 
-          
+        int dis = max(abs(sx - fx), abs(sy - fy));
 
-        if (abs(sx - fx) <= t && abs(sy - fy) <= t) {
-            return true;
+        if (dis == 0) {
+            return t != 1;
         } 
-                   
+                             
 
-        return false;
+        return (dis <= t);
     }
 };
