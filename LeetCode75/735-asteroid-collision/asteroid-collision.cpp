80f2a128db3f019dce90ae82e5c6d795f2445e6a
Atharva Patilpate <atharva.patilpate@gmail.com>
2023-11-10 22:54:19 +0530

    Time: 10 ms (64.18%) | Memory: 17.7 MB (87.61%) - LeetSync

---
+++ LeetCode75/735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    vector<int> asteroidCollision(vector<int>& ast) {
+        vector<int> s;
+
+        for(auto& i : ast) {
+
+            if(!s.empty() && s.back() > 0 && i < 0) {
+                int flag = 1;
+            
+                while(!s.empty() && s.back() > 0 && i < 0) {
+            
+                    if(abs(s.back()) < abs(i)) {
+                        s.pop_back();
+                    }
+            
+                    else if(abs(s.back()) == abs(i)) {
+                        s.pop_back();
+                        flag = 0;
+                        break;
+                    }
+            
+                    else {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if(flag) {
+                    s.push_back(i);
+                }
+            }
+            
+            else {
+                s.push_back(i);
+            }
+        }
+
+        return s;
+    }
+};
