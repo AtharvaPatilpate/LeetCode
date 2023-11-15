26af5158438f1216915356ae75b1dc034763a8ce
Atharva Patilpate <atharva.patilpate@gmail.com>
2023-11-15 18:49:04 +0530

    Time: 32 ms (58.67%) | Memory: 35.3 MB (8.48%) - LeetSync

--- LeetCode75/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ LeetCode75/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -12,11 +12,7 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if (!root) {
-            return nullptr;
-        }
-        
-        if (root->val == val) {
+        if (!root || root->val == val) {
             return root;
         }
 
