83b84e52237944c488671bfd965d828e69490dcd
Atharva Patilpate <atharva.patilpate@gmail.com>
2023-11-16 10:12:58 +0530

    Time: 3 ms (74.62%) | Memory: 10.3 MB (98.79%) - LeetSync

--- DailyProblem/2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ DailyProblem/2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -3,7 +3,7 @@ public:
     string findDifferentBinaryString(vector<string>& nums) {
         string ans;
 
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); i++) {
             ans += (nums[i][i] == '0' ? '1' : '0');
         }
 
